3e14c3efb6beb22306a890f8989ab7e2718cc4ce
Luxurate <109758129+Luxurate@users.noreply.github.com>
2023-04-27 03:14:32 +0530

    Only the current user who matches with the item he sent can delete the image / post. if currentuserid = image uid then he can delete the post

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fooddonation/screens/home_page.dart';
 
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
