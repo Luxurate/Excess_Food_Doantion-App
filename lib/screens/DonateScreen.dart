f0b50bcd0e3da3f0658ae63f3f6db654b904e395
Luxurate <109758129+Luxurate@users.noreply.github.com>
2023-05-23 20:03:29 +0530

    +

--- lib/screens/DonateScreen.dart
+++ lib/screens/DonateScreen.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      title: 'Firebase Storage Demo',
+      title: 'Firebase Storage ',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
