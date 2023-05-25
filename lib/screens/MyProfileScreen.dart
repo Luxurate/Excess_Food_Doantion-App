033626bbf1c6d2e0ed38c0433719d9922f67dfe2
Luxurate <109758129+Luxurate@users.noreply.github.com>
2023-05-26 00:09:38 +0530

    +

--- lib/screens/MyProfileScreen.dart
+++ lib/screens/MyProfileScreen.dart
@@ -69,7 +69,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Profile picture uploaded successfully!'),
+          content: Text('Profile picture uploaded!'),
         ),
       );
     } catch (e) {
