ccba712e4c9a2b6fbbbd4663bae95702798b3cf3
Luxurate <109758129+Luxurate@users.noreply.github.com>
2023-05-27 14:47:03 +0530

    +

--- lib/screens/UsersScreen.dart
+++ lib/screens/UsersScreen.dart
@@ -20,11 +20,21 @@ class UsersListScreen extends StatelessWidget {
 
                 final email = userData['email'] as String? ?? 'N/A';
 
-
                 return ListTile(
-
-                  subtitle: Text(email),
-
+                  subtitle: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChatScreen(
+                            currentUserEmail: 'currentuser@example.com',
+                            tappedUserEmail: email,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Text(email),
+                  ),
                 );
               },
             );
@@ -34,6 +44,122 @@ class UsersListScreen extends StatelessWidget {
             return Center(child: CircularProgressIndicator());
           }
         },
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  final String currentUserEmail;
+  final String tappedUserEmail;
+
+  ChatScreen({required this.currentUserEmail, required this.tappedUserEmail});
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _messageController = TextEditingController();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  late CollectionReference _messagesCollection;
+
+  @override
+  void initState() {
+    super.initState();
+    _messagesCollection = _firestore.collection('messages');
+  }
+
+  Future<void> _sendMessage(String message) async {
+    try {
+      await _messagesCollection.add({
+        'sender': widget.currentUserEmail,
+        'receiver': widget.tappedUserEmail,
+        'message': message,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+      _messageController.clear();
+    } catch (e) {
+      // Handle error
+      print('Error sending message: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat with ${widget.tappedUserEmail}'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: _messagesCollection
+                  .where('sender', isEqualTo: widget.currentUserEmail)
+                  .where('receiver', isEqualTo: widget.tappedUserEmail)
+                  .orderBy('timestamp', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final messages = snapshot.data!.docs;
+                  return ListView.builder(
+                    reverse: true,
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      final messageData =
+                      messages[index].data() as Map<String, dynamic>;
+                      final sender = messageData['sender'] as String?;
+                      final message = messageData['message'] as String?;
+                      final isCurrentUser =
+                          sender == widget.currentUserEmail;
+
+                      return ListTile(
+                        title: Text(message ?? ''),
+                        subtitle: Text(
+                          sender ?? '',
+                          style: TextStyle(
+                            fontWeight: isCurrentUser
+                                ? FontWeight.bold
+                                : FontWeight.normal,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      hintText: 'Type a message...',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    final message = _messageController.text.trim();
+                    if (message.isNotEmpty) {
+                      _sendMessage(message);
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
