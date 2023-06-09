e452b2520bd129bea9e1766b1bcc113233f308a1
Luxurate <109758129+Luxurate@users.noreply.github.com>
2023-06-09 20:38:21 +0530

    +

--- lib/screens/RatingScreen.dart
+++ lib/screens/RatingScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RatingPage extends StatefulWidget {
   const RatingPage({Key? key}) : super(key: key);
@@ -8,23 +9,45 @@ class RatingPage extends StatefulWidget {
   _RatingPageState createState() => _RatingPageState();
 }
 
-class _RatingPageState extends State<RatingPage> {
+class _RatingPageState extends State<RatingPage> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
   int _rating = 0;
   String _feedback = '';
+  late String _userEmail;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _getUserEmail();
+  }
+
+  void _getUserEmail() {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      setState(() {
+        _userEmail = currentUser.email ?? '';
+      });
+    }
+  }
 
   void _setRating(int rating) {
     setState(() {
       _rating = rating;
     });
+    _animationController.forward(from: 0);
   }
 
   void _submitFeedback() {
     FirebaseFirestore.instance.collection('ratings').add({
       'rating': _rating,
       'feedback': _feedback,
+      'userEmail': _userEmail,
     }).then((value) {
       print('Feedback submitted');
-      // Reset the rating and feedback fields
       setState(() {
         _rating = 0;
         _feedback = '';
@@ -39,12 +62,18 @@ class _RatingPageState extends State<RatingPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Rating Page'),
+        backgroundColor: Colors.purple,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Text(
+              'Email: $_userEmail',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
             Text(
               'Rate the app:',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
@@ -53,7 +82,11 @@ class _RatingPageState extends State<RatingPage> {
               children: List.generate(5, (index) {
                 final starColor = index < _rating ? Colors.yellow : Colors.grey;
                 return IconButton(
-                  icon: Icon(Icons.star, color: starColor),
+                  icon: AnimatedIcon(
+                    icon: AnimatedIcons.event_add,
+                    color: starColor,
+                    progress: _animationController,
+                  ),
                   onPressed: () => _setRating(index + 1),
                 );
               }),
@@ -63,26 +96,90 @@ class _RatingPageState extends State<RatingPage> {
               'Write your feedback:',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  _feedback = value;
-                });
-              },
-              maxLines: 5,
-              decoration: InputDecoration(
-                hintText: 'Enter your feedback here',
-                border: OutlineInputBorder(),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.purple, width: 1),
+              ),
+              child: TextField(
+                onChanged: (value) {
+                  setState(() {
+                    _feedback = value;
+                  });
+                },
+                maxLines: 5,
+                decoration: InputDecoration(
+                  hintText: 'Enter your feedback here',
+                  contentPadding: const EdgeInsets.all(10),
+                  border: InputBorder.none,
+                ),
               ),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _submitFeedback,
-              child: Text('Submit Feedback'),
+            Center(
+              child: ElevatedButton(
+                onPressed: _submitFeedback,
+                child: Text('Submit Feedback'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Ratings:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection('ratings').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+
+                  final ratings = snapshot.data?.docs ?? [];
+
+                  return ListView.builder(
+                    itemCount: ratings.length,
+                    itemBuilder: (context, index) {
+                      final ratingData = ratings[index].data() as Map<String, dynamic>;
+                      final rating = ratingData['rating'];
+                      final feedback = ratingData['feedback'];
+                      final userEmail = ratingData['userEmail'];
+
+                      return ListTile(
+                        title: Text('Rating: $rating'),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Feedback: $feedback'),
+                            Text('User: $userEmail'),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
